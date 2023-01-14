4e2bc5b045c82aebf95e1ae8f9c985895620791c
Aneesh <aneeshx000@gmail.com>
2023-01-14 21:13:22 +0530

    Updates

---
+++ precache-manifest.a92f02a29da3c9ec03b44febaa0f79e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c47fa1adff6dc6e5e20e",
+    "url": "/Portfolio-1.1/static/css/main.e38327cf.chunk.css"
+  },
+  {
+    "revision": "c47fa1adff6dc6e5e20e",
+    "url": "/Portfolio-1.1/static/js/main.c47fa1ad.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/Portfolio-1.1/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "2c688c5ee996890065aa",
+    "url": "/Portfolio-1.1/static/js/2.2c688c5e.chunk.js"
+  },
+  {
+    "revision": "00d7fdf5299d8bc6f763",
+    "url": "/Portfolio-1.1/static/js/runtime~main.00d7fdf5.js"
+  },
+  {
+    "revision": "6c71ea55103554d870c983b751a9c3e8",
+    "url": "/Portfolio-1.1/static/media/pic1.6c71ea55.jpg"
+  },
+  {
+    "revision": "677736124081b301faf4c74b71814e8c",
+    "url": "/Portfolio-1.1/static/media/p6.67773612.webp"
+  },
+  {
+    "revision": "7180b885391d6c1ac839c0bc76a0ab93",
+    "url": "/Portfolio-1.1/static/media/p3.7180b885.png"
+  },
+  {
+    "revision": "4f02bbe475ef804862b07ec0abd23842",
+    "url": "/Portfolio-1.1/index.html"
+  }
+];
