2d818d15fd5d4879977a4d160f6c06c38381a7a1
Aneesh <aneeshx000@gmail.com>
2023-02-09 14:00:03 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Portfolio from "./Components/Portfolio";
