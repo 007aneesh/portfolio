f1c6786393a350d12ca0e2b1a9f86319f553a87a
Aneesh <aneeshx000@gmail.com>
2023-02-13 10:44:39 +0530

    Updates

---
+++ precache-manifest.1b2b7e1e1da2d2f297246b8c923b00c5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe16d668466c57d36990",
+    "url": "/portfolio/static/css/main.f95644d8.chunk.css"
+  },
+  {
+    "revision": "fe16d668466c57d36990",
+    "url": "/portfolio/static/js/main.2bb4456a.chunk.js"
+  },
+  {
+    "revision": "daaca62150442b9474da",
+    "url": "/portfolio/static/js/runtime~main.eb1093dd.js"
+  },
+  {
+    "revision": "31c0065f5e45952c9715",
+    "url": "/portfolio/static/css/2.a953d3f4.chunk.css"
+  },
+  {
+    "revision": "31c0065f5e45952c9715",
+    "url": "/portfolio/static/js/2.0f245f21.chunk.js"
+  },
+  {
+    "revision": "9e2d897a76c17d18ff55",
+    "url": "/portfolio/static/js/3.3bc8c1cd.chunk.js"
+  },
+  {
+    "revision": "6f646925be38a3033ad01dcc2d2a7348",
+    "url": "/portfolio/static/media/pic2.6f646925.jpg"
+  },
+  {
+    "revision": "fd62581fd82299abe89673abf5779854",
+    "url": "/portfolio/static/media/aneesh2.fd62581f.png"
+  },
+  {
+    "revision": "7180b885391d6c1ac839c0bc76a0ab93",
+    "url": "/portfolio/static/media/p3.7180b885.png"
+  },
+  {
+    "revision": "f592b5837d8803e40e26a968cbef0b07",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e066c64a14ceb87d636a25a3cc0d6ef8.js"
+  "/portfolio/precache-manifest.1b2b7e1e1da2d2f297246b8c923b00c5.js"
 );
 
 workbox.clientsClaim();
